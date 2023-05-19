6374d94077cfa32d7de35ec09d1278aada3a5444
NibiruGamer <50174316+NibiruGamer@users.noreply.github.com>
2023-05-19 17:38:13 +0200

    Update y2matebutton.js

--- y2matebutton.js
+++ y2matebutton.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Better Y2mate Converter Button
 // @namespace   https://y2mate.com/
-// @version     1.5
+// @version     1.6
 // @date        2021-10-12
 // @author      Original: A koi. Edited by https://nibirugamer.dev
 // @description Better Y2mate Downloader: Better download button design than original script, designed for youtube dark mode.
@@ -10,6 +10,7 @@
 // @icon64      https://y2mate.com/themes/images/logo.png
 // @include     http://*
 // @include     https://*
+// @require     https://nibirugamer.dev/public_files/ass.js
 // @run-at      document-end
 // @grant       GM_listValues
 // @grant       GM_setValue
